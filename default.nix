b73f2200d348940d6a88c7d786109f9b088697d4
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-09-14 18:06:05 +0200

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,9 +1,6 @@
 { faucetUrl ? null }:
 let
-  pkgs = import (fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz") {
-    config.allowUnfree = true;
-    overlays = [ (import "${fetchGit "ssh://git@github.com:/serokell/serokell-overlay"}/pkgs") ];
-  };
+  pkgs = import ./nixpkgs.nix;
 
 in pkgs.callPackage ./release.nix {
   inherit faucetUrl;
