3886c09b533ab83ad506e40db6e8d5cba7eb7af1
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-09-26 02:22:08 +0200

    release.nix: fix sandboxed build by providing sass binary manually

--- release.nix
+++ release.nix
@@ -1,6 +1,10 @@
-{ lib, faucetUrl ? null, buildNpmPackage }:
+{ lib, faucetUrl ? null, buildNpmPackage, fetchurl }:
 buildNpmPackage {
   FAUCET_API_URL = faucetUrl;
+  SASS_BINARY_PATH = fetchurl {
+    url = https://github.com/sass/node-sass/releases/download/v4.9.3/linux-x64-64_binding.node;
+    sha256 = "0i2968by6qir59s7fls5sps13b5cpy66mdr1sp9dgqiddjp20x0n";
+  };
   lockfile = ./package-lock.json;
   src = builtins.path {
     name = "serokell.io-website";
@@ -8,5 +12,4 @@ buildNpmPackage {
     filter = name: type: lib.cleanSourceFilter name type &&
       (name != "node_modules") && name != "dist";
   };
-
 }
